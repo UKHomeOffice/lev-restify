d8f36597f71ffc8b4716149a655a93dc6d74ba1f
Daniel A.C. Martin <github@daniel-martin.co.uk>
2019-05-14 16:13:56 +0100

    Add extra formatters for serving HTML

--- src/index.js
+++ src/index.js
@@ -18,6 +18,8 @@ const createServer = options => {
 
   const name = options.name;
   const log = logger(name);
+  const formatBinary = restify.formatters['application/octet-stream; q=0.2'];
+  const formatText = restify.formatters['text/plain; q=0.3'];
 
   if (name) {
     process.title = name.replace(/[^\w]/gi, '').substr(0, 6);
@@ -25,7 +27,26 @@ const createServer = options => {
 
   const httpd = originalCreateServer(Object.assign({
     log: log
-  }, options));
+  }, options, {
+    formatters: Object.assign({
+      'application/gzip': formatBinary,
+      'application/pdf': formatBinary,
+      'application/xhtml+xml': formatText,
+      'application/zip': formatBinary,
+      'font/otf': formatBinary,
+      'font/ttf': formatBinary,
+      'font/woff': formatBinary,
+      'font/woff2': formatBinary,
+      'image/gif': formatBinary,
+      'image/jpeg': formatBinary,
+      'image/png': formatBinary,
+      'image/svg+xml': formatText,
+      'image/x-icon': formatBinary,
+      'text/css': formatText,
+      'text/html': formatText,
+      'text/javascript': formatText
+    }, options.formatters)
+  }));
 
   httpd.log = log;
   httpd.promiseRejectionHandler = promiseRejectionHandler(log),
