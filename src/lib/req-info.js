74e2b1de9874d5bd5a417e784b6cf6644b3e51ea
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-01-23 20:23:54 +0000

    [LEVWEB-1323] Simplify req-info implementation

--- src/lib/req-info.js
+++ src/lib/req-info.js
@@ -1,13 +1,8 @@
 'use strict';
 
-module.exports = (req) => {
-  const aud = req.headers['x-auth-audience'];
-  const client = aud && aud.split(',').filter(e => e !== 'lev-api')[0];
-
-  return ({
-    username: req.headers['x-auth-username'],
-    client: client,
-    groups: req.headers['x-auth-groups'] && String(req.headers['x-auth-groups']).split(',') || [],
-    roles: req.headers['x-auth-roles'] && String(req.headers['x-auth-roles']).split(',') || []
-  });
-};
+module.exports = (req) => ({
+  username: req.headers['x-auth-username'],
+  client: req.headers['x-auth-audience'] && String(req.headers['x-auth-audience']).split(',').filter(e => e !== 'lev-api')[0],
+  groups: req.headers['x-auth-groups'] && String(req.headers['x-auth-groups']).split(',') || [],
+  roles: req.headers['x-auth-roles'] && String(req.headers['x-auth-roles']).split(',') || []
+});
