1cb6087e47a5aabad47a75d4b2fb4a8e9b22f9c6
aarongill1 <aaron.gill@digital.homeoffice.gov.uk>
2023-11-22 15:20:00 +0000

    Updated user to be from header added by adapter if it exists

--- src/lib/req-info.js
+++ src/lib/req-info.js
@@ -3,9 +3,10 @@
 module.exports = (req) => {
   const aud = req.headers['x-auth-aud'] || req.headers['x-auth-audience'];
   const client = aud && aud.split(',').filter(e => e !== 'lev-api')[0];
+  const user = req.headers['x-user-id'] || req.headers['x-auth-username'];
 
   return ({
-    username: req.headers['x-auth-username'],
+    username: user,
     client: client,
     groups: req.headers['x-auth-groups'] && String(req.headers['x-auth-groups']).split(',') || [],
     roles: req.headers['x-auth-roles'] && String(req.headers['x-auth-roles']).split(',') || []
