f68f0dbd3af9b1497aa0b8331264a64952aae046
aarongill1 <aaron.gill@digital.homeoffice.gov.uk>
2023-11-27 15:48:02 +0000

    added unit tests for x-original-username header being used

--- src/lib/req-info.js
+++ src/lib/req-info.js
@@ -3,7 +3,9 @@
 module.exports = (req) => {
   const aud = req.headers['x-auth-aud'] || req.headers['x-auth-audience'];
   const client = aud && aud.split(',').filter(e => e !== 'lev-api')[0];
-  const user = req.headers['x-user-id'] || req.headers['x-auth-username'];
+
+  // x-original-username header set by lev-adapter. Once this is 'wired' the gatekeeper generated x-auth-username will be removed
+  const user = req.headers['x-original-username'] || req.headers['x-auth-username'];
 
   return ({
     username: user,
