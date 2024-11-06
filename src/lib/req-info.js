56f5abd610d6cab9dfc57b8754d82c400b1c9a41
Ashley Bickerstaff <ashley.bickerstaff@kainos.com>
2024-11-06 16:32:44 +0000

    LEV-651 - Handling internal group headers from the Adapter

--- src/lib/req-info.js
+++ src/lib/req-info.js
@@ -7,10 +7,20 @@ module.exports = (req) => {
   // x-original-username header set by lev-adapter. Once this is 'wired' the gatekeeper generated x-auth-username will be removed
   const user = req.headers['x-original-username'] || req.headers['x-auth-username'];
 
+  let groups = req.headers['x-auth-groups'] && String(req.headers['x-auth-groups']).split(',') || [];
+  try {
+    const parsed = JSON.parse(req.headers['x-groups-internal'].toString());
+    if (Array.isArray(parsed)) {
+      groups = groups.concat(parsed);
+    }
+  } catch (e) {
+    // Don't case if this fails.
+  }
+
   return ({
     username: user,
     client: client,
-    groups: req.headers['x-auth-groups'] && String(req.headers['x-auth-groups']).split(',') || [],
+    groups: groups,
     roles: req.headers['x-auth-roles'] && String(req.headers['x-auth-roles']).split(',') || []
   });
 };
